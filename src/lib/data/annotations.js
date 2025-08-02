e9be187f939908bc87aacdc548abd21594a28b6c
Gabe Coulter <coultergabriel@gmail.com>
2025-08-02 23:34:33 +0100

    Update scrolling annotations

--- src/lib/data/annotations.js
+++ src/lib/data/annotations.js
@@ -4,11 +4,94 @@ export const annotationMap = {
         women: ''
     },
     '2013-12': {
-        men: 'First monthly record, Blaire H jumps 5.8m, about as high as the walls on a two story house.',
+        men: 'Setting the first monthly WOO leaderboard record, Blaire H hits 5.8m, about as high as the walls on a two story house.',
         women: ''
     },
-    '2014-01': {
-        men: 'Tester',
+    '2015-01': {
+        men: "The UK's Lewis Crathern, future 2016 Vice Big Air World Champion, is first to break 15m, jumping about as high as the length of two London Buses end-to-end.",
         women: ''
-    }
+    },
+    '2015-06': {
+        men: "Fluid Kiteboarding owner, Gilion Goveia, is the first to break 20m using his own brand's kite, a 6m Fluid ATV, a huge milestone in record progression.",
+        women: ''
+    },
+    // '2017-11': {
+    //     men: "His third entry into the record books, South Africa's Joshua Emanuel comes up just shy of the 30m mark, hitting 29m in Cape Town.",
+    //     women: ''
+    // }
+    '2018-11': {
+        men: "Mirroring his 2015 record of 19.1 m, Dutch-born Gijs Wassenaar flies just shy of a milestone 30 m in Zandvoort, Netherlands, with a 29.6 m effort...",
+        women: ''
+    },
+    '2019-01': {
+        men: "However, that threshold is quickly smashed by fellow Dutchman, Maarten Haeger, reaching a staggering 32 m in the Big Air mecca of Misty Cliffs, Cape Town.",
+        women: ''
+    },
+    '2019-11': {
+        men: "A flurry of new records are set at the tail end of 2019 and into 2020, with Maarten Haeger and Mike 'GetHighWithMike' MacDonald trading new heights towards 35 m, off the shorelines of Cape Town...",
+        women: ''
+    },
+    '2020-01': {
+        men: "With Maarten reclaiming his title in January 2020, hitting 34.8m ...",
+        women: "Ending Karen Hou's 9-record-long streak, Lithuanian Gabby Pioraite enters the leaderboards, extending the record towards 25, at 23.6m at Kite Beach."
+    },
+    // '2020-01': {
+    //     men: "... with Maarten coming up just short at 34.8 m in January of 2020.",
+    //     women: ''
+    // },
+    '2020-08': {
+        men: "A record that would stand...",
+        women: ''
+    },
+    '2021-03': {
+        men: "For three...",
+        women: ''
+    },
+    '2021-10': {
+        men: "... entire...",
+        women: "But as sure as the sun will rise, and the wind will blow, Karen Hou returns, just 0.4m short of 25m in her home waters of Longmen Beach in Penghu Island."
+    },
+    '2022-05': {
+        men: "... years...",
+        women: ''
+    },
+    '2023-02': {
+        men: "... with the 35 m barrier finally being broken by the Netherlands' Jamie Overbeek.",
+        women: ''
+    },
+    '2023-08': {
+        men: "With his fourth entry into the record books, Joshua Emanuel is the first to break 36m, soaring 11 stories high at 36.2m...",
+        women: ''
+    },
+    '2024-09': {
+        men: "A record that stands for just over a year, when the young Kiwi, Hugo Wigglesworth, hits 36.7m where the record stands today.",
+        women: ''
+    },
+
+
+    '2015-01': {
+        men: '',
+        women: "Kicking off the women's leaderboard, Tina Jerke sets a solid foundation of 7.9m in January 2015..."
+    },
+    '2015-02': {
+        men: '',
+        women: "A record almost instantly broken by Gerdian Mjnten, smashing through 10m to a 13.9m height - imagine a large shpping container, longways-up - higher than that."
+    },
+    '2015-10': {
+        men: '',
+        women: "October 2015, enter Jia Lin 'Karen' Hou from Penghu Island, Taiwan, breaking the 15m barrier at 15.6m. Remember this name."
+    },
+    '2018-02': {
+        men: '',
+        women: "Already her 7th entry into the record books, Karen Hou breaks the 20m barrier on the famous Kite Beach in Cape Town, breaking her own record for the 6th time at 20.5m."
+    },
+    '2022-11': {
+        men: '',
+        women: "November 2022, the 25m barrier is finally broken by France's Angely Bouillot, reaching 25.1m in the Misty Cliff waters of Cape Town. Here is another one to remember."
+    },
+    '2023-12': {
+        men: '',
+        women: "Over a year after her previous record, Angely sets another, again smashing through a milestone, this time 30m. The 32.6m jump brings the womens leaderboard within the range of the mens, an unsurprising achievement from Bouillot, the only female competitor to ever ridfe in Red Bull's 'King of the Air' event, inspiring the introduction of a Women's category. This massive 32.6m jump, the largest single increase in the women's leaderboard, is where the record stands today.",
+    },
+
 };
